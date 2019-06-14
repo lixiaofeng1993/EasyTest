0f3559d342d08287ac356989585fb8e72195dcca
lixiaofeng1993 <liyongfeng@xxbmm.com>
2019-06-14 11:30:35 +0800

    '饼图优化'

--- lib/public.py
+++ lib/public.py
@@ -183,31 +183,45 @@ def DrawPie(pass_num=0, fail=0, error=0):
     now_time = int(time.mktime(time.strptime(start_time, '%Y-%m-%d %H:%M:%S')))
     mpl.rcParams[u'font.sans-serif'] = ['simhei']
     mpl.rcParams['axes.unicode_minus'] = False
+    # 调节图形大小，宽，高
+    plt.figure(figsize=(6, 9))
+    # 定义饼状图的标签，标签是列表
     labels = 'pass', 'fail', 'error'
+    # 每个标签占多大，会自动去算百分比
     my_labels = [pass_num, fail, error]
     colors = ['green', 'orange', 'red']
-    explode = [0.1, 0, 0]  # 0.1 凸出这部分，
-    plt.axes(aspect=1)  # set this , Figure is round, otherwise it is an ellipse
-    # autopct ，show percet
-    patches, l_text, p_text = plt.pie(x=my_labels, colors=colors, labels=labels, explode=explode,
-                                      autopct='%3.1f %%', shadow=True, labeldistance=1.1, startangle=90,
-                                      pctdistance=0.6)
+    # 将某部分爆炸出来， 使用括号，将第一块分割出来，数值的大小是分割出来的与其他两块的间隙
+    explode = (0.05, 0, 0)
+
+    patches, l_text, p_text = plt.pie(my_labels, explode=explode, labels=labels, colors=colors,
+                                      labeldistance=1.1, autopct='%3.1f%%', shadow=False,
+                                      startangle=90, pctdistance=0.6, textprops={'fontsize': 12, 'color': 'w'})
+
+    # labeldistance，文本的位置离远点有多远，1.1指1.1倍半径的位置
+    # autopct，圆里面的文本格式，%3.1f%%表示小数有三位，整数有一位的浮点数
+    # shadow，饼是否有阴影
+    # startangle，起始角度，0，表示从0开始逆时针转，为第一块。一般选择从90度开始比较好看
+    # pctdistance，百分比的text离圆心的距离
+    # patches, l_texts, p_texts，为了得到饼图的返回值，p_texts饼图内部文本的，l_texts饼图外label的文本
+
+    # 改变文本的大小
     # 方法是把每一个text遍历。调用set_size方法设置它的属性
     for t in l_text:
         t.set_size = (30)
     for t in p_text:
         t.set_size = (20)
     plt.title('Running results of test cases')
-    # plt.show()
     # 显示图例,去掉重复的标签
     colors, labels = plt.gca().get_legend_handles_labels()
     by_labels = OrderedDict(zip(labels, colors))
-    plt.legend(by_labels.values(), by_labels.keys())
-    # plt.legend()
+    plt.legend(by_labels.values(), by_labels.keys(), loc='upper left')
+    # 设置x，y轴刻度一致，这样饼图才能是圆的
+    plt.axis('equal')
+    # plt.show()
     # 保存饼图
     pic_path = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'media')
     imgPath = os.path.join(pic_path, str(now_time) + "pie.png")
-    plt.savefig(imgPath, bbox_inches='tight')
+    plt.savefig(imgPath)
     plt.tight_layout()
     plt.cla()  # 不覆盖
     return str(now_time) + "pie.png"
