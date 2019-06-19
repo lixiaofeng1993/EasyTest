97ae57d0762b6929a195e370d40a755cf891153f
lixiaofeng1993 <18701137212@163.com>
2019-06-20 00:22:48 +0800

    KeyError session

--- EasyTest/views.py
+++ EasyTest/views.py
@@ -66,8 +66,11 @@ def login_action(request):
             request.session['user'] = username  # 将session信息记录到浏览器
             user_ = User.objects.get(username=username)
             request.session['user_id'] = user_.id  # 将session信息记录到浏览器
-            if request.session['login_from'] == '//':
-                request.session['login_from'] = '/index/'
+            try:
+            	if request.session['login_from'] == '//':
+	                request.session['login_from'] = '/index/'
+            except KeyError as e:
+            	request.session['login_from'] = '/index/'
             response = redirect(request.session['login_from'])
             # response = HttpResponseRedirect('/index/')
             log.info('用户： {} 登录成功！'.format(username))
