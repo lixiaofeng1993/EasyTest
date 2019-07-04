7fcba4c49fd6b90367ae22ae0f82a728ea96962d
lixiaofeng1993 <liyongfeng@xxbmm.com>
2019-07-04 14:38:01 +0800

    flower配置文件44

--- common/connectMySql.py
+++ common/connectMySql.py
@@ -19,46 +19,47 @@ class SqL:
         self.to_connect(self.job)
 
     def to_connect(self, job):
-        if job:
-            try:
-                self.conn = pymysql.connect(host=readConfig.MySQL_host_job, database=readConfig.MySQL_database_job,
-                                            user=readConfig.MySQL_user_job,
-                                            password=readConfig.MySQL_pwd_job, port=int(readConfig.MySQL_port_job),
-                                            use_unicode=True, charset='utf8')
-                # log.info('job 数据库连接成功')
-            except Exception as e:
-                log.error('job 数据库链接异常! {}'.format(e))
-        else:
-            try:
-                self.conn = pymysql.connect(host=readConfig.MySQL_host, database=readConfig.MySQL_database,
-                                            user=readConfig.MySQL_user,
-                                            password=readConfig.MySQL_pwd, port=int(readConfig.MySQL_port),
-                                            use_unicode=True, charset='utf8')
-                # log.info('本地 数据库连接成功')
-            except Exception as e:
-                log.error('本地 数据库链接异常! {}'.format(e))
+        while True:
+            if job:
+                try:
+                    self.conn = pymysql.connect(host=readConfig.MySQL_host_job, database=readConfig.MySQL_database_job,
+                                                user=readConfig.MySQL_user_job,
+                                                password=readConfig.MySQL_pwd_job, port=int(readConfig.MySQL_port_job),
+                                                use_unicode=True, charset='utf8')
+                    break
+                    # log.info('job 数据库连接成功')
+                except Exception as e:
+                    log.error('job 数据库链接异常! {}'.format(e))
+                    continue
+            else:
+                try:
+                    self.conn = pymysql.connect(host=readConfig.MySQL_host, database=readConfig.MySQL_database,
+                                                user=readConfig.MySQL_user,
+                                                password=readConfig.MySQL_pwd, port=int(readConfig.MySQL_port),
+                                                use_unicode=True, charset='utf8')
+                    break
+                    # log.info('本地 数据库连接成功')
+                except Exception as e:
+                    log.error('本地 数据库链接异常! {}'.format(e))
+                    continue
 
     def reconnect(self, dict_type, sql):
-        while True:
-            try:
-                self.conn.ping(reconnect=True)  # pymysql.err.InterfaceError: (0, '')
-                log.info('数据库已经连接成功！')
-            except:
-                log.warning('数据库已断开，重连中...')
-                self.to_connect(self.job)
-            if dict_type:  # 返回数据字典类型
-                cur = self.conn.cursor(cursor=pymysql.cursors.DictCursor)
-            else:
-                cur = self.conn.cursor()
-            try:
-                with cur as cur:
-                    cur.execute(sql)  # 执行sql
-                break
-            except Exception as e:
-                self.conn.rollback()
-                log.error('执行SQL语句出现异常：{}'.format(e))
-                self.reconnect(dict_type, sql)
-        return cur
+        try:
+            self.conn.ping()  # pymysql.err.InterfaceError: (0, '')
+        except:
+            log.warning('数据库已断开，重连中...')
+            self.to_connect(self.job)
+        if dict_type:  # 返回数据字典类型
+            cur = self.conn.cursor(cursor=pymysql.cursors.DictCursor)
+        else:
+            cur = self.conn.cursor()
+        try:
+            with cur as cur:
+                cur.execute(sql)  # 执行sql
+            return cur
+        except Exception as e:
+            self.conn.rollback()
+            log.error('执行SQL语句出现异常：{}'.format(e))
 
     def execute_sql(self, sql, dict_type=False, num=1):
         """返回查询结果集
@@ -67,6 +68,9 @@ class SqL:
             num： 返回的数据是一个还是多个
         """
         cur = self.reconnect(dict_type, sql)
+        if not cur:
+            log.error('sql执行，查询数据失败！！！')
+            return None
         try:
             if 'delete' in sql or 'insert' in sql or 'update' in sql:
                 self.conn.commit()  # 提交
