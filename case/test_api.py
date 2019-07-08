e00a0314a6a54d37b517c0890d1f95c70fb0fed3
lixiaofeng1993 <liyongfeng@xxbmm.com>
2019-07-08 18:48:31 +0800

    unitest8

--- case/test_api.py
+++ case/test_api.py
@@ -14,22 +14,26 @@ test_data = get_json(demo_path)
 log = Log()  # 初始化log
 log.info('--------------------------------test_data: {}'.format(test_data))
 
+
 @ddt.ddt
 class Test_api(unittest.TestCase):
-	@classmethod
-	def setUpClass(cls):
-		cls.s = requests.session()
+    @classmethod
+    def setUpClass(cls):
+        cls.s = requests.session()
 
-	@ddt.data(*test_data)
-	def test_api(self, data):
-		"""{0}"""
-		res = base_api.send_requests(self.s, data)  # 调用send_requests方法,请求接口,返回结果
-		checkpoint = data["checkpoint"]  # 检查点 checkpoint
-		res_text = res["text"]  # 返回结果
-		text = json.loads(res_text)
-		for inspect in checkpoint:
-			self.assertTrue(inspect["expect"] in str(text[inspect["check"]]).lower(), "检查点验证失败！")  # 断言
+    @ddt.data(*test_data)
+    def test_api(self, data):
+        """{0}"""
+
+        log.info('2222222222222222222222222: {}'.format(data))
+
+        # res = base_api.send_requests(self.s, data)  # 调用send_requests方法,请求接口,返回结果
+        # checkpoint = data["checkpoint"]  # 检查点 checkpoint
+        # res_text = res["text"]  # 返回结果
+        # text = json.loads(res_text)
+        # for inspect in checkpoint:
+        # 	self.assertTrue(inspect["expect"] in str(text[inspect["check"]]).lower(), "检查点验证失败！")  # 断言
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
