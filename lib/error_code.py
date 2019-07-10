d492dc99a2fd8aacd664a4c2da39f2521ad8c0c2
lixiaofeng1993 <18701137212@163.com>
2019-07-10 23:55:02 +0800

    'error-code'

---
+++ lib/error_code.py
@@ -0,0 +1,16 @@
+#!/user/bin/env python
+# coding=utf-8
+'''
+# 创 建 人: 李先生
+# 文 件 名: error_code.py
+# 说   明: 
+# 创建时间: 2019/7/10 21:43
+'''
+
+class ErrorCode:
+
+    interface_error = '系统异常，检查接口参数是否正确！'
+
+    index_error = 'list索引错误，接口返回list长度不够！'
+
+    validators_error = '未设置检查点！'
