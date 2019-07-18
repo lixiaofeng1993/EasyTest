901b3993fe9711bc37bacc7cdfbf1eab660cdc7c
lixiaofeng1993 <liyongfeng@xxbmm.com>
2019-07-18 17:21:38 +0800

    天气6

--- EasyTest/views.py
+++ EasyTest/views.py
@@ -94,7 +94,7 @@ def get_whether(request):
         now = str(datetime.datetime.now())[:10]
         item = result_dict.get('weatherinfo')  # 取字典的值用get方法
         item['now'] = now
-        return JsonResponse(item)
+        return HttpResponse(item)
 
 
 # 登录
