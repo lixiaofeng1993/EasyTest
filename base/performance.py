69087af476e8efee7a41f2a8dd424c6b3274a167
lixiaofeng1993 <liyongfeng@xxbmm.com>
2019-08-27 15:13:08 +0800

    启动locust按钮9

--- base/performance.py
+++ base/performance.py
@@ -8,7 +8,7 @@
 '''
 import sys
 
-sys.path.insert(0, r'/home/lixiaofeng/EasyTest')
+sys.path.insert(0, r'/var/lib/jenkins/workspace/EasyTest')
 sys.path.insert(0, r'D:\EasyTest')
 
 from locust import HttpLocust, TaskSet, task, Locust, events
