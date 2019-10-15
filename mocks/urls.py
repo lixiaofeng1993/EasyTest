fb6372482bb641dc6d62b8020de7e8c36acd3a18
lixiaofeng1993 <liyongfeng@xxbmm.com>
2019-10-15 14:03:50 +0800

    '部署mock成功'

--- mocks/urls.py
+++ mocks/urls.py
@@ -13,7 +13,7 @@ from . import views
 
 urlpatterns = [
     # 项目
-    url(r'api/', view=views.mock_api, name='mock_api'),
+    url(r'api/?', view=views.mock_api, name='mock_api'),
     url(r'[\w|\/]+', view=views.dispatch_request, name='dispatch_request'),
 
 ]
