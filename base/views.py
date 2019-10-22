2fa60c10d406ef88a373597aa36b9e80de99282e
lixiaofeng1993 <liyongfeng@xxbmm.com>
2019-10-22 12:54:34 +0800

    '优化5'

--- base/views.py
+++ base/views.py
@@ -1824,7 +1824,7 @@ def findata(request):
                     data = f.readlines()
             for i in data:
                 data_list.append(i.replace('True', 'true').replace('False', 'false').replace('None', 'null'))
-            log.info('data_listdata_listdata_listdata_listdata_listdata_list==========================={}'.format(data_list))
+            log.info('data_listdata_listdata_listdata_listdata_listdata_list==========================={}'.format(log_file))
             return JsonResponse(data_list, safe=False)
         if get_type == 'get_task_log':
             task_log_path = '/var/celery_logs/celery_worker_err.log'
