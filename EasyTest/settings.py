f9b39b2030a6cc3953dc514e3e876a3800e14ef0
lixiaofeng1993 <18701137212@163.com>
2020-01-13 21:53:37 +0800

    '后台管理优化'

--- EasyTest/settings.py
+++ EasyTest/settings.py
@@ -140,12 +140,6 @@ SESSION_COOKIE_AGE = 60 * 300  # 30分钟
 SESSION_SAVE_EVERY_REQUEST = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # 关闭浏览器，则COOKIE失效
 
-# simpleui 设置
-SIMPLEUI_STATIC_OFFLINE = True  # 访问静态文件
-# SIMPLEUI_HOME_PAGE = 'http://39.105.136.231'
-# SIMPLEUI_HOME_TITLE = 'EasyTest-后台管理'
-SIMPLEUI_HOME_ICON = 'fa fa-user'
-
 # 上传
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # 对应文件夹
 if not os.path.exists(MEDIA_ROOT): os.mkdir(MEDIA_ROOT)
@@ -317,3 +311,146 @@ REST_FRAMEWORK = {
     # 'PAGE_SIZE': 10,
     'PAGINATE_BY': 10,
 }
+
+# simpleui 设置
+
+# 首页配置
+# SIMPLEUI_HOME_PAGE = 'https://www.baidu.com'
+# 首页标题
+# SIMPLEUI_HOME_TITLE = '百度一下你就知道'
+# 首页图标,支持element-ui的图标和fontawesome的图标
+# SIMPLEUI_HOME_ICON = 'el-icon-date'
+
+# 设置simpleui 点击首页图标跳转的地址
+# SIMPLEUI_INDEX = 'https://www.88cto.com'
+
+# 首页显示服务器、python、django、simpleui相关信息
+# SIMPLEUI_HOME_INFO = True
+
+# 首页显示快速操作
+# SIMPLEUI_HOME_QUICK = True
+
+# 首页显示最近动作
+# SIMPLEUI_HOME_ACTION = True
+
+# 自定义SIMPLEUI的Logo
+# SIMPLEUI_LOGO = 'https://avatars2.githubusercontent.com/u/13655483?s=60&v=4'
+
+# 登录页粒子动画，默认开启，False关闭
+# SIMPLEUI_LOGIN_PARTICLES = False
+
+# 让simpleui 不要收集相关信息
+# SIMPLEUI_ANALYSIS = True
+
+# 自定义simpleui 菜单
+SIMPLEUI_CONFIG = {
+    # 在自定义菜单的基础上保留系统模块
+    'system_keep': False,
+    'menus': [
+        {
+            'app': ' auth',
+            'name': '账户管理',
+            # 'icon': 'fas fa-user-shield',
+            'models': [{
+                'name': '用户',
+                'icon': 'fa fa-user',
+                'url': 'auth/user/'
+            }, {
+                'name': '组',
+                'icon': 'fas fa-users-cog',
+                'url': 'auth/group/'
+            }
+            ]
+        },
+        {
+            'app': ' base',
+            'name': '测试平台',
+            # 'icon': 'fas fa-user-shield',
+            'models': [
+                {
+                    'name': '项目管理',
+                    # 'icon': 'fa fa-product-hunt fa-fw',
+                    'url': 'base/project/'
+                },
+                {
+                    'name': '测试环境',
+                    # 'icon': 'fa fa-product-hunt fa-fw',
+                    'url': 'base/environment/'
+                },
+                {
+                    'name': '接口管理',
+                    # 'icon': 'fa fa-product-hunt fa-fw',
+                    'url': 'base/interface/'
+                },
+                {
+                    'name': '测试用例',
+                    # 'icon': 'fa fa-product-hunt fa-fw',
+                    'url': 'base/case/'
+                },
+                {
+                    'name': '测试计划',
+                    # 'icon': 'fa fa-product-hunt fa-fw',
+                    'url': 'base/plan/'
+                },
+                {
+                    'name': '签名管理',
+                    # 'icon': 'fa fa-product-hunt fa-fw',
+                    'url': 'base/sign/'
+                },
+                {
+                    'name': '测试报告',
+                    # 'icon': 'fa fa-product-hunt fa-fw',
+                    'url': 'base/report/'
+                }, {
+                    'name': '发布会',
+                    # 'icon': 'fa fa-product-hunt fa-fw',
+                    'url': 'base/event/'
+                }, {
+                    'name': '发布会嘉宾',
+                    # 'icon': 'fa fa-product-hunt fa-fw',
+                    'url': 'base/guest/'
+                },
+            ]
+        },
+        {
+            'app': 'djcelery',
+            'name': '定时任务',
+            'icon': 'fas fa-user-shield',
+            'models': [{
+                'name': 'Crontabs',
+                # 'icon': 'fa fa-user',
+                'url': 'djcelery/crontabschedule/'
+            }, {
+                'name': 'Intervals',
+                # 'icon': 'fa fa-user',
+                'url': 'djcelery/intervalschedule/'
+            }, {
+                'name': 'Periodic tasks',
+                # 'icon': 'fa fa-user',
+                'url': 'djcelery/periodictask/'
+            }, {
+                'name': ' Tasks',
+                # 'icon': 'fa fa-user',
+                'url': 'djcelery/taskstate/'
+            }, {
+                'name': 'Workers',
+                # 'icon': 'fa fa-user',
+                'url': 'djcelery/workerstate/'
+            },
+            ]
+        },
+    ]
+}
+# 是否显示默认图标，默认=True
+# SIMPLEUI_DEFAULT_ICON = False
+
+# 图标设置，图标参考：
+SIMPLEUI_ICON = {
+    # '测试平台': 'fab fa-apple',
+    # '账户管理': 'fas fa-user-tie'
+}
+
+# 指定simpleui 是否以脱机模式加载静态资源，为True的时候将默认从本地读取所有资源，即使没有联网一样可以。适合内网项目
+# 不填该项或者为False的时候，默认从第三方的cdn获取
+
+SIMPLEUI_STATIC_OFFLINE = True
