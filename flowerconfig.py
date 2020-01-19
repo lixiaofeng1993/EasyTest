8ac9c5df60e9d55d737697ccea03ec80ef7f6a2e
lixiaofeng1993 <18701137212@163.com>
2020-01-20 00:08:32 +0800

    'flower2'

--- flowerconfig.py
+++ flowerconfig.py
@@ -7,12 +7,11 @@
 # @Software: PyCharm
 
 # Broker settings
-BROKER_URL = 'amqp:redis://127.0.0.1:6379/0'
+BROKER_URL = 'amqp:redis://localhost:6379/0'
 
 # RabbitMQ management api
 broker_api = 'http://guest:guest@39.105.136.231:15672/api/'
 
-basic_auth = 'lixiaofeng:123456'
 # Enable debug logging
 logging = 'INFO'
 
